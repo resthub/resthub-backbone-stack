08af918061a589bc27e50b7ea54e2a5252b0f1be
Baptiste Meurant <baptiste.meurant@gmail.com>
2012-08-22 16:57:56 +0200

    add handlebars and configure it with requirejs 2.0. related to #2 and fixes #3

--- js/main.js
+++ js/main.js
@@ -1,28 +1,35 @@
 // Set the require.js configuration for your application.
 require.config({
 
-  // Libraries
-  paths: {
-    jquery: "libs/jquery",
-    underscore: "libs/underscore",
-    'underscore.string': "libs/underscore.string",
-    backbone: "libs/backbone",
-    localstorage: "libs/localstorage",
-    use: "libs/use",
-    text: "libs/text",
-    i18n: "libs/i18n",
-    pubsub: "libs/resthub/pubsub"
-  },
-  locale: localStorage.getItem('locale') || 'en-us'
+    shim:{
+        'handlebars':{
+            exports:'Handlebars'
+        }
+    },
+
+    // Libraries
+    paths:{
+        jquery:"libs/jquery",
+        underscore:"libs/underscore",
+        'underscore.string':"libs/underscore.string",
+        backbone:"libs/backbone",
+        localstorage:"libs/localstorage",
+        use:"libs/use",
+        text:"libs/text",
+        i18n:"libs/i18n",
+        pubsub:"libs/resthub/pubsub",
+        handlebars:"libs/handlebars"
+    },
+    locale:localStorage.getItem('locale') || 'en-us'
 });
 
 // Load our app module and pass it to our definition function
-require(['underscore', 'underscore.string', 'view/app'], function(_, _s, App){
-    
-  // Merge Underscore and Underscore.String
-  _.str = _s;
-  _.mixin(_.str.exports());
-  _.str.include('Underscore.string', 'string');
-  
-  App.initialize();
+require(['underscore', 'underscore.string', 'view/app'], function (_, _s, App) {
+
+    // Merge Underscore and Underscore.String
+    _.str = _s;
+    _.mixin(_.str.exports());
+    _.str.include('Underscore.string', 'string');
+
+    App.initialize();
 });
