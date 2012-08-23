280121638097f8ebb7923b987b16c64006726769
Baptiste Meurant <baptiste.meurant@gmail.com>
2012-08-24 00:45:30 +0200

    add keymaster. fixes #8

--- js/main.js
+++ js/main.js
@@ -38,21 +38,22 @@ require.config({
 
     // Libraries
     paths:{
-        jquery:"libs/jquery",
-        underscore:"libs/underscore",
-        'underscore.string':"libs/underscore.string",
-        backbone:"libs/backbone",
-        localstorage:"libs/localstorage",
-        text:"libs/text",
-        i18n:"libs/i18n",
-        pubsub:"resthub/pubsub",
-        handlebars:"libs/handlebars",
-        'handlebars-helpers':"libs/resthub/handlebars-helpers",
+        jquery:'libs/jquery',
+        underscore:'libs/underscore',
+        'underscore.string':'libs/underscore.string',
+        backbone:'libs/backbone',
+        localstorage:'libs/localstorage',
+        text:'libs/text',
+        i18n:'libs/i18n',
+        pubsub:'resthub/pubsub',
+        handlebars:'libs/handlebars',
+        'handlebars-helpers':'libs/resthub/handlebars-helpers',
         'bootstrap':'libs/bootstrap',
         'backbone-validation':'resthub/backbone-validation.ext',
         'backbone-queryparams':'libs/backbone.queryparams',
         'backbone-paginator':'libs/backbone.paginator',
-        async:'libs/async.js'
+        async:'libs/async.js',
+        keymaster:'libs/keymaster'
     }
 });
 
