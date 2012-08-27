e6ababa00d3e3060c3a25265e86d69b3bdd065aa
Baptiste Meurant <baptiste.meurant@gmail.com>
2012-08-27 15:48:34 +0200

    remove pubsub and provide a simple Backbone.Events extension. adapt pubsub tests

--- tests/pubsub.js
+++ tests/pubsub.js
@@ -1,89 +1,95 @@
-require(['pubsub'], function(Pubsub) {
-    	
-	var listeners = [];
-	
-	module('pusbub', {
-		teardown: function() {
-			$.each(listeners, function(i, listener) {
-				Pubsub.unsubscribe(listener);
-			});
-			
-			listeners  = [];
-		},
-		setup: function() {
-			localStorage.clear();
-		}
-	});
+require(['pubsub', 'underscore'], function (Pubsub, _) {
 
-	test('should publish triggers to all listeners', function() {
-		listeners.push(Pubsub.subscribe('test-event', function () {
-			ok(true, 'test-event triggered twice');
-		}));
-		listeners.push(Pubsub.subscribe('test-event', function () {
-			ok(true, 'test-event triggered twice');
-		}));
+    var listeners = [];
 
-		// When triggering 'myEvent'
-		Pubsub.publish('test-event');
-		
-		
-	});
-	
-	test('should not publish to other listeners', 1, function() {
-		listeners.push(Pubsub.subscribe('test-event', function () {
-			ok(true, 'test-event triggered twice');
-		}));
-		listeners.push(Pubsub.subscribe('test-event-2', function () {
-			ok(true, 'test-event-2 triggered twice');
-		}));
+    module('pusbub', {
+        teardown:function () {
+            Pubsub.off();
+        },
+        setup:function () {
+            localStorage.clear();
+        }
+    });
 
-		// When triggering 'myEvent1'
-		Pubsub.publish('test-event');
-	});
-	
-	
-	test('should publish but prevent unsubscribed listeners', 1, function() {
-		var guid = Pubsub.subscribe('test-unsuscribe', function () {
-			ok(false, 'test-event-2 not triggered when unsubscribed');
-		});
-		
-		listeners.push(Pubsub.subscribe('test-unsuscribe', function () {
-			ok(true, 'test-event triggered twice');
-		}));
-		
-		// unscribe one of the two listeners
-		Pubsub.unsubscribe(guid);
-		
-		// When triggering 'myEvent1'
-		Pubsub.publish('test-unsuscribe');
-		
-	});
-	
-	
-	test('should publish with data parameters', function() {		
-		var data = {should: 'be', good: 'enough'},
-		arr = ['should', 'be', 'good', 'enough'];
-		
-		listeners.push(Pubsub.subscribe('test-publish-data', function(o) {
-			deepEqual(o, data, 'listeners get data parameters');
-		}));
-		
-		Pubsub.publish('test-publish-data', [data]);
-		
-		listeners.push(Pubsub.subscribe('test-publish-data-mutliple', function() {
-			var arr = Array.prototype.slice.call(arguments);			
-			equal(arr.join(' '), 'should be good enough', 'listeners can get a list of parameters too.');
-		}));
+    test('should publish triggers to all listeners', 2, function () {
+        Pubsub.on('test-event', function () {
+            ok(true, 'test-event triggered twice');
+        }, this);
+        Pubsub.on('test-event', function () {
+            ok(true, 'test-event triggered twice');
+        }, this);
+
+        // When triggering 'myEvent'
+        Pubsub.trigger('test-event');
 
 
-		Pubsub.publish('test-publish-data-mutliple', arr);
-	});
-	
-	
-	test('should not lost binding', 1, function() {
-		var o = {foo: 'bar', test: function() { deepEqual(this, o, 'this is O !'); }};
-		listeners.push(Pubsub.subscribe('test-binding', $.proxy(o.test, o)));
-		Pubsub.publish('test-binding');
-	});
-		
+    });
+
+    test('should not publish to other listeners', 1, function () {
+        Pubsub.on('test-event', function () {
+            ok(true, 'test-event triggered once');
+        }, this);
+        Pubsub.on('test-event-2', function () {
+            ok(true, 'test-event-2 not triggered');
+        }, this);
+
+        // When triggering 'myEvent1'
+        Pubsub.trigger('test-event');
+    });
+
+
+    test('should publish but prevent unsubscribed listeners', 1, function () {
+
+        var Subscriber1 = function () {
+            Pubsub.on('test-unsubscribe', function () {
+                ok(false, 'test-event-2 not triggered when unsubscribed');
+            }, this);
+        };
+
+        var subscriber1 = new Subscriber1();
+
+        var Subscriber2 = function () {
+            Pubsub.on('test-unsubscribe', function () {
+                ok(true, 'test-event triggered once');
+            }, this);
+        };
+
+        new Subscriber2();
+
+        // unsubscribe one of the two listeners
+        Pubsub.off(null, null, subscriber1);
+
+        // When triggering 'myEvent1'
+        Pubsub.trigger('test-unsubscribe');
+
+    });
+
+
+    test('should publish with data parameters', 2, function () {
+        var data1 = "data1",
+            data2 = "data2";
+
+        Pubsub.on('test-publish-data', function (o) {
+            deepEqual(o, data1, 'listeners get data parameters');
+        }, this);
+
+        Pubsub.trigger('test-publish-data', data1);
+
+        Pubsub.on('test-publish-data-multiple', function () {
+            var arr = Array.prototype.slice.call(arguments);
+            equal(arr.join(' '), 'data1 data2', 'listeners can get a list of parameters too.');
+        }, this);
+
+        Pubsub.trigger('test-publish-data-multiple', data1, data2);
+    });
+
+
+    test('should not lost binding', 1, function () {
+        var o = {foo:'bar', test:function () {
+            deepEqual(this, o, 'this is O !');
+        }};
+        Pubsub.on('test-binding', $.proxy(o.test, o), this);
+        Pubsub.trigger('test-binding');
+    });
+
 });
