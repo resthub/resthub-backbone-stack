391ba2db8e4db8532136c8ad5e255ba30b52bd24
Sébastien Deleuze <seb@deleuze.fr>
2012-09-20 16:54:17 +0200

    Simplify test and make it easy to test

--- js/router.js
+++ js/router.js
@@ -3,14 +3,15 @@ define(['backbone', 'backbone-queryparams'], function(Backbone) {
     var AppRouter = Backbone.Router.extend({
         
         initialize: function() {
-            Backbone.history.start({ pushState: true, root: "/" });
+            Backbone.history.start();
         },
         
         routes: {
-            '*path': 'defaultAction'
+            'test': 'test'
         },
 
-        defaultAction: function() {
+        test: function() {
+            alert("test");
         }
     });
     
