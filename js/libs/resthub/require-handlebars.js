ede94f20cc630eb198469c3a5ed9570b1434caff
Johan Poirier <johan.poirier@gmail.com>
2012-10-11 13:47:58 +0200

    Made somme changes to hbs module in order to pass r.js optimization.

--- js/libs/resthub/require-handlebars.js
+++ js/libs/resthub/require-handlebars.js
@@ -1,23 +1,24 @@
 define(['module'], function(module) {
 
-  var masterConfig = module.config();
+    var masterConfig = (module.config && module.config()) || {};
 
-  var hbs = {
-    load: function (name, req, load, config) {
-      config = config || {};
-      var extension = masterConfig.extension;
-      if (config.extension) {
-        extension = config.extension;
-      }
-      extension = extension || 'hbs';
-      var textName = 'text!' + name + '.' + extension;
+    var hbs = {
+        load: function (name, req, load, config) {
+            config = config || {};
+            
+            var extension = masterConfig.extension;
+            if (config.extension) {
+                extension = config.extension;
+            }
+            extension = extension || 'hbs';
+            var textName = 'text!' + name + '.' + extension;
 
-      return req(['handlebars', textName], function (Handlebars, template) {
-          load(Handlebars.compile(template));
+            return req(['handlebars', textName], function (Handlebars, template) {
+                load((Handlebars ? Handlebars.compile(template) : template));
+            }
+            );
         }
-      );
-    }
-  };
+    };
 
-  return hbs;
+    return hbs;
 });
