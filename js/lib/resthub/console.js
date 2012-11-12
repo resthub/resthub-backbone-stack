10b800dfc7c984aaca446f0bb0db6b6dff4d30ab
Sébastien Deleuze <seb@deleuze.fr>
2012-11-12 18:03:39 +0100

    Log management fixes Fix a typo in moment.js fr locale file

--- js/lib/resthub/console.js
+++ js/lib/resthub/console.js
@@ -9,37 +9,28 @@ define(['jquery'], function ($) {
     var console = window.console;
 
     // manage IE8 & 9
-    var methods = ["log","info","warn","error","assert","dir","clear","profile","profileEnd"];
-    if (Function.prototype.bind && console && typeof console.log == "object") {
+    var methods = ['log','info','warn','error','assert','dir','clear','profile','profileEnd'];
+    if (Function.prototype.bind && console && typeof console.log === 'object') {
         methods.forEach(function (method) {
             console[method] = this.call(console[method], console);
         }, Function.prototype.bind);
     }
 
-    console.buffer = [];
-    console.BUFFER_MAX_SIZE = 10;
-    // override if needed
-    console.serverUrl = "api/logs";
-
-    // Helper to define a logging level object; helps with optimisation.
-    var defineLogLevel = function(value, name) {
-        return { value: value, name: name };
-    };
-
-    var Levels= {};
-    Levels.DEBUG = defineLogLevel(1, 'DEBUG');
-    Levels.INFO = defineLogLevel(2, 'INFO');
-    Levels.WARN = defineLogLevel(4, 'WARN');
-    Levels.ERROR = defineLogLevel(8, 'ERROR');
-    Levels.OFF = defineLogLevel(99, 'OFF');
-    console.Levels = Levels;
-
-    //default level
-    console.level = Levels.DEBUG;
-
-    var enabledFor = function (lvl) {
-        var filterLevel = console.level;
-        return lvl.value >= filterLevel.value;
+    // Can be customized if needed
+    console.serverUrl = 'api/log';
+    console.levels = ['debug', 'info', 'warn', 'error'];
+    // Disabled by default
+    console.level = 'off';
+    
+    var enabledFor = function (level) {
+        if(console.level === 'off') return false;
+        if (_.indexOf(console.levels, level) === -1) {
+            throw new Error('Invalid log level "' + strategy + '", must be one of ' + JSON.stringify(console.levels));
+        }
+        if (_.indexOf(console.levels, console.level) === -1) {
+            throw new Error('Invalid log console.level "' + strategy + '", must be one of ' + JSON.stringify(console.levels));
+        }
+        return _.indexOf(console.levels, level) >= _.indexOf(console.levels, console.level);
     };
 
     var log = console.log;
@@ -47,65 +38,62 @@ define(['jquery'], function ($) {
     var info = console.info;
     var warn = console.warn;
     var error = console.error;
+    
+    // console.log = console.debug
     console.log = function () {
         log.apply(this, Array.prototype.slice.call(arguments));
-        //sendToServer("error", arguments);
+        if(enabledFor('debug')){
+            sendLogToServer('debug', arguments);
+        }
     };
     console.debug = function () {
         debug.apply(this, Array.prototype.slice.call(arguments));
-        if(enabledFor(Levels.DEBUG)){
-            bufferLog("DEBUG", arguments);
+        if(enabledFor('debug')){
+            sendLogToServer('debug', arguments);
         }
     };
     console.info = function () {
         info.apply(this, Array.prototype.slice.call(arguments));
-        if(enabledFor(Levels.INFO)){
-            bufferLog("INFO", arguments);
+        if(enabledFor('info')){
+            sendLogToServer('info', arguments);
         }
     };
     console.warn = function () {
         warn.apply(this, Array.prototype.slice.call(arguments));
-        if(enabledFor(Levels.WARN)){
-            bufferLog("WARN", arguments);
+        if(enabledFor('warn')){
+            sendLogToServer('warn', arguments);
         }
     };
     console.error = function () {
         error.apply(this, Array.prototype.slice.call(arguments));
-        if(enabledFor(Levels.ERROR)){
-            bufferLog("ERROR", arguments);
+        if(enabledFor('error')){
+            sendLogToServer('error', arguments);
         }
     };
 
-    var bufferLog = function(level, msg){
-        var message = typeof msg === "object" ? JSON.stringify(msg) : msg;
-        var log = {"level":level, "message": message, "time": new Date()};
-        console.buffer.push(log);
-        if(console.buffer.length == console.BUFFER_MAX_SIZE){
-            // could some logs be lost ?
-            sendLogsToServer();
-        }
-    };
 
-    var sendLogsToServer = function(){
+    var sendLogToServer = function(level, msg){
+        if((typeof msg === 'object') && (msg.length === 1) && (typeof msg[0] === 'string')){
+            var message = msg[0];
+        } else {
+            var message = JSON.stringify(msg);
+        }
+
+        var log = {'level':level, 'message': message, 'time': new Date()};
+        
         $.ajax({
             type: 'POST',
             url: console.serverUrl,
-            data: JSON.stringify(console.buffer),
-            contentType:"application/json",
+            data: JSON.stringify(log),
+            contentType:'application/json',
             success: function(){
                 // update log level
             }
         });
-        console.buffer = [];
-    }
-
-    // on page change/reload send the content of the buffer
-    window.onunload = function(){
-        sendLogsToServer();
-    }
+    };
 
     // manage global JS errors
     window.onerror = function(message, url, linenumber){
-        bufferLog("ERROR", "JavaScript error: " + message + " on line " + linenumber + " for " + url);
-    }
+        sendLogToServer('error', 'JavaScript error: ' + message + ' on line ' + linenumber + ' for ' + url);
+    };
 });
